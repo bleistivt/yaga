e56a86d05d4630bb6efba49859feb0ec9eaf22cf
bleistivt <me@bleistivt.net>
2020-02-18 22:54:53 +0100

    library classes are autoloaded already

--- bootstrap.php
+++ bootstrap.php
@@ -1,12 +1,3 @@
 <?php if (!defined('APPLICATION')) exit();
 
-// Register Yaga library classes and interfaces in the autoloader
-$map = Gdn_Autoloader::MAP_LIBRARY;
-$context = Gdn_Autoloader::CONTEXT_APPLICATION;
-$path = PATH_PLUGINS.'/yaga/library';
-$options = ['Extension' => 'yaga'];
-
-Gdn_Autoloader::start();
-Gdn_Autoloader::registerMap($map, $context, $path, $options);
-
 require_once(PATH_PLUGINS.'/yaga/library/functions.render.php');
