50338bfdc0abcd9c98558508dc6fabc7b0d48e5d
bleistivt <me@bleistivt.net>
2020-02-28 12:54:03 +0100

    Delete duplicates before adding unique index

--- settings/structure.php
+++ settings/structure.php
@@ -48,6 +48,18 @@ if ($construct->tableExists()) {
 }
 $construct->reset();
 
+// Delete duplicates from GDN_Reactions that violate the UNIQUE constraint (user reacting to the same content twice).
+$sql->query("
+    delete from ${px}YagaReaction
+    where ReactionID in (
+        select * from (
+            select max(r.ReactionID)
+            from ${px}YagaReaction as r
+            group by r.InsertUserID, r.ParentID, r.ParentType
+            having count(r.ReactionID) > 1
+        ) as r2
+)", 'delete');
+
 // Tracks the data associated with reacting to content
 $construct->table('YagaReaction')
     ->primaryKey('ReactionID')
@@ -59,9 +71,9 @@ $construct->table('YagaReaction')
     ->column('DateInserted', 'datetime', false, 'index.Record')
     ->set($explicit, $drop);
 
-/*$result = $sql->query("SHOW INDEX FROM ${Px}Reaction WHERE Key_name = 'IX_ParentID_ParentType'")->result(); 
+/*$result = $sql->query("SHOW INDEX FROM ${px}Reaction WHERE Key_name = 'IX_ParentID_ParentType'")->result(); 
 if (!$result && !$construct->CaptureOnly) {
-    $sql->query("ALTER TABLE ${Px}Reaction ADD INDEX IX_ParentID_ParentType (ParentID, ParentType)");
+    $sql->query("ALTER TABLE ${px}Reaction ADD INDEX IX_ParentID_ParentType (ParentID, ParentType)");
 }*/
 
 // Describes actions that can be taken on a comment, discussion or activity
